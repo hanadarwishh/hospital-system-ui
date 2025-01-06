94b0a55404a371e6db4dace07a06b057f3e7b82e
hanadarwishh <hana.darwish@ngu.edu.eg>
2025-01-06 10:15:24 +0200

    Updates

--- src/components/Image.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Image = ({ src, alt, width, height, style }) => {
-  return (
-    <img src={src} alt={alt} width={width} height={height} style={style} />
-  );
-};
-
-export default Image;
